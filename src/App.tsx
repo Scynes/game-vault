2b055a62f4c626e756f1b4dbc922d2c0a84c12f7
Scynes <dustin.greyfield@gmail.com>
2023-08-15 16:37:02 -0400

    wip: skeleton responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 const App = () => {
     return (
-        <>
-        </>
+        <Grid templateAreas={{ base: `'nav' 'main'`, lg: `'nav nav' 'aside main'` }}>
+            <GridItem area={'nav'} bg={'coral'}>
+                Navigation
+            </GridItem>
+            <Show above="lg">
+                <GridItem area={'aside'} bg={'crimson'}>
+                    Aside
+                </GridItem>
+            </Show>
+            <GridItem area={'main'} bg={'green'}>
+                Main
+            </GridItem>
+        </Grid>
     );
 }
 
