1e79eb9b6228feeeff703e8ef1fc3e76a7386922
Scynes <dustin.greyfield@gmail.com>
2023-08-17 12:31:12 -0400

    wip(GameCard): added basic card ui

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -1,9 +1,14 @@
+import { Card, CardBody, Heading, Image } from '@chakra-ui/react';
 import Game from '@interfaces/Game';
 
 const GameCard = (game : Game) => {
     return (
-        <>
-        </>
+        <Card>
+            <Image src={ game.background_image } />
+            <CardBody>
+                <Heading>{ game.name }</Heading>
+            </CardBody>
+        </Card>
     );
 }
 
