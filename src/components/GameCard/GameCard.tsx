9f962ce9669782e9804d15fbfcd255640aef88ab
Scynes <dustin.greyfield@gmail.com>
2023-08-17 12:47:20 -0400

    wip(GameGallery): added GameCard implementation instead of a list

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -1,7 +1,21 @@
 import { Card, CardBody, Heading, Image } from '@chakra-ui/react';
 import Game from '@interfaces/Game';
 
-const GameCard = (game : Game) => {
+/**
+ * Describes the expected properties for the GameCard component.
+ */
+interface Properties {
+    // The game object containing information about the game.
+    game: Game;
+}
+
+/**
+ * A component that displays information about a game in a card format.
+ *
+ * @param {Properties} props - The properties containing the game information.
+ * @returns {JSX.Element} - The JSX element representing the GameCard component.
+ */
+const GameCard = ({ game }: Properties) => {
     return (
         <Card>
             <Image src={ game.background_image } />
--- src/components/GameGallery/GameGallery.tsx
+++ src/components/GameGallery/GameGallery.tsx
@@ -1,3 +1,6 @@
+import { SimpleGrid } from "@chakra-ui/react";
+
+import GameCard from "@components/GameCard";
 import useGamesCollection from "@hooks/useGamesCollection";
 
 const GameGallery = () => {
@@ -5,11 +8,11 @@ const GameGallery = () => {
     const { collection } = useGamesCollection();
 
     return (
-        <ul>
-            { collection.map((game, index) => (
-                <li key={ index }>{ game.name }</li>
+        <SimpleGrid columns={ 3 } spacing={ '1rem' }>
+            { collection.map( (game) => (
+                <GameCard game={ game } key={ game.id }/>
             )) }
-        </ul>
+        </SimpleGrid>
     );
 }
 
